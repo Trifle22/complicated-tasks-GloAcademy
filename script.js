4dcc5695bb7467b296765254a37556b8352aa612
trifle22 <xromchenkov99@gmail.com>
2021-02-01 14:15:02 +0300

    3/3

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 'use strict';
 
-let lang = 'ru';
+let lang = 'en';
 
-// if (lang === 'ru') {
-//   console.log('Понедельник, ','Вторник, ','Среда, ','Четверг, ','Пятница, ','Суббота, ','Воскресенье');
-// } else if (lang === 'en') {
-//   console.log('Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, ','Sunday');
-// }
+let arr = [['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'],
+          ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']];
 
+
+/*
+if (lang === 'ru') {
+  console.log('Понедельник, ','Вторник, ','Среда, ','Четверг, ','Пятница, ','Суббота, ','Воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, ','Sunday');
+}
+*/
+
+/*
 switch(lang) {
   case 'ru':
     console.log('Понедельник, ','Вторник, ','Среда, ','Четверг, ','Пятница, ','Суббота, ','Воскресенье');
     break;
   case 'en':
     console.log('Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, ','Sunday');
+    break;
 }
+*/
+
+console.log((lang === 'ru') ? arr[0].join(', ') : arr[1].join(', '));
+
+
