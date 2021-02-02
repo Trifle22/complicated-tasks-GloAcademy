72d08d39ea1f0112cc85998a220a740a3fa2fdbc
Trifle22 <xromchenkov99@gmail.com>
2021-02-02 16:03:37 +0300

    lesson04

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
 'use strict';
 
-let lang = 'en';
+let string = prompt();
 
-let arr = [['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'],
-          ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']];
-
-
-/*
-if (lang === 'ru') {
-  console.log('Понедельник, ','Вторник, ','Среда, ','Четверг, ','Пятница, ','Суббота, ','Воскресенье');
-} else if (lang === 'en') {
-  console.log('Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, ','Sunday');
+function changeString(str) {
+  if (typeof str === 'string') {
+    str = str.trim();
+    if (str.length > 31) {
+      str = str.replace(str.slice(31, str.length), '...');
+    }
+    return str;
+  } else {
+    return 'Передана не строка';
+  }
 }
-*/
 
-/*
-switch(lang) {
-  case 'ru':
-    console.log('Понедельник, ','Вторник, ','Среда, ','Четверг, ','Пятница, ','Суббота, ','Воскресенье');
-    break;
-  case 'en':
-    console.log('Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, ','Sunday');
-    break;
-}
-*/
-
-console.log((lang === 'ru') ? arr[0].join(', ') : arr[1].join(', '));
-
-
+console.log(changeString(string));
