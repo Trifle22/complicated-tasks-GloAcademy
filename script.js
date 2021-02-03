2e99755e89f2f3100544c83ce9b84d100426903a
Trifle22 <xromchenkov99@gmail.com>
2021-02-03 15:07:39 +0300

    lesson05

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 'use strict';
 
-let string = prompt('Введите что-либо');
+let arr = [];
 
-function changeString(str) {
-  if (!isNaN(str)) {
-    return 'Передана не строка';
-  } else {
-    str = str.trim();
-    if (str.length > 31) {
-      str = str.replace(str.slice(31, str.length), '...');
-    }
-    return str;
+for (let i = 0; i < 7; i++) {
+  arr.push(prompt('Введите многозначное число'));
+}
+
+for (let i = 0; i < arr.length; i++) {
+  if ((arr[i][0] === '2' || arr[i][0] === '4') && (arr[i].length >= 2)) {
+    console.log(arr[i]);
   }
 }
 
-console.log(changeString(string));
+function isPrime(num) {
+  for (let i = 2, max = Math.sqrt(num); i <=max; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num >= 1;
+}
 
+let primes = [];
+
+function getPrimes(num) {
+  for (let i = 1; i <= num; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+getPrimes(100);
+
+primes.forEach(item => {
+  console.log(`${item}, делится на 1 и ${item}`);
+});
