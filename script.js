ac215079e26e63e2e66cb472b3bfff2728c8f2a2
Trifle22 <xromchenkov99@gmail.com>
2021-02-06 12:49:14 +0300

    lesson07

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
 'use strict';
+let date = new Date();
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
 
-let arr = [];
-
-for (let i = 0; i < 7; i++) {
-  arr.push(prompt('Введите многозначное число'));
-}
-
-for (let i = 0; i < arr.length; i++) {
-  if ((arr[i][0] === '2' || arr[i][0] === '4') && (arr[i].length >= 2)) {
-    console.log(arr[i]);
+week.forEach(function(item) {
+  if (item  === 'Суббота' || item === 'Воскресение') {
+    item = item.italics();
   }
-}
-
-function isPrime(num) {
-  for (let i = 2, max = Math.sqrt(num); i <=max; i++) {
-    if (num % i === 0) {
-      return false;
-    }
+  if (item === week[date.getDay() - 1]) {
+    item = item.bold();
   }
-  return num >= 1;
-}
+  document.write(item + '<br></br>');
 
-let primes = [];
-
-function getPrimes(num) {
-  for (let i = 1; i <= num; i++) {
-    if (isPrime(i)) {
-      primes.push(i);
-    }
-  }
-  return primes;
-}
-
-getPrimes(100);
-
-primes.forEach(item => {
-  console.log(`${item}, делится на 1 и ${item}`);
 });
+
